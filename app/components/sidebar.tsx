0b5328a49308ca3b5b25fb9699f2448e2228e7ca
Mustafa-raja <mustafa.raja.here@gmail.com>
2024-10-21 21:20:03 +0500

    sidebar fixed

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -1,11 +1,20 @@
 import React from "react";
-import { Home, Building2, Users, Mail, Calendar, MessageSquare, Settings, HelpCircle} from 'lucide-react'
-
+import {
+  Home,
+  Building2,
+  Users,
+  Mail,
+  Calendar,
+  MessageSquare,
+  Settings,
+  HelpCircle,
+} from "lucide-react";
+import Image from "next/image";
 
 function Sidebar() {
   return (
-    <div className="hidden lg:flex flex-col w-16 bg-primary text-primary-content">
-      <div className="flex-1 flex flex-col items-center justify-start pt-5 gap-6">
+    <div className="hidden lg:flex flex-col w-16  text-primary-content mt-4 ">
+      <div className="flex flex-col items-center justify-start p-5 gap-8 bg-primary rounded-r-xl">
         <Home />
         <Building2 />
         <Users />
@@ -15,10 +24,15 @@ function Sidebar() {
         <Settings />
         <HelpCircle />
       </div>
-      <div className="pb-5">
+      <div className="flex-1 flex items-end p-5">
         <div className="avatar online">
           <div className="w-10 rounded-full">
-            <img src="/placeholder.svg?height=40&width=40" alt="User avatar" />
+            <Image
+              src="/placeholder.svg?height=40&width=40"
+              alt="User avatar"
+              height={20}
+              width={20}
+            />
           </div>
         </div>
       </div>
