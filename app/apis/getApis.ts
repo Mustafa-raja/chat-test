6d0068ab0dc844ca0a5597f7d4898479c2efca81
Mustafa-raja <mustafa.raja.here@gmail.com>
2024-10-22 03:11:32 +0500

    api

--- app/apis/getApis.ts
+++ app/apis/getApis.ts
@@ -1,5 +1,6 @@
 export async function getUsers() {
-  const url = "https://cors-anywhere.herokuapp.com/http://18.143.79.95/api/chatSystem/users/list";
+  const url =
+    "http://localhost:8080/http://18.143.79.95/api/chatSystem/users/list";
 
   try {
     const response = await fetch(url, {
@@ -22,25 +23,48 @@ export async function getUsers() {
 }
 
 export async function getGroups() {
-    const url = "https://cors-anywhere.herokuapp.com/http://18.143.79.95/api/chatSystem/groups/list";
-  
-    try {
-      const response = await fetch(url, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-  
-      if (!response.ok) {
-        throw new Error(`Error: ${response.status}`);
-      }
-  
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error("There was an error with the fetch operation:", error);
-      return null;
+  const url =
+    "http://localhost:8080/http://18.143.79.95/api/chatSystem/groups/list";
+
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
     }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("There was an error with the fetch operation:", error);
+    return null;
   }
-  
+}
+
+export async function getChatByUser(userId: number) {
+  const url = `http://localhost:8080/http://18.143.79.95/api/chatSystem/chatByUserId/${userId}`;
+
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("There was an error with the fetch operation:", error);
+    return null;
+  }
+}
